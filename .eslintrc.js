5a75128efcae42d26ea1ff55e3612eec0333bb20
wuweijiange <490104722@qq.com>
2021-12-17 15:13:22 +0800

    feat-Configuration ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 13,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    // 'comma-dangle': [2, 'never'], // 对象字面量项尾不能有逗号
+    'no-func-assign': 2, // 禁止重复的函数声明
+    'no-unreachable': 2, // 不能有无法执行的代码
+  },
+};
