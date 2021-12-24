b08761363df93c647b46dbf133c44f92170f3399
wuweijiange <490104722@qq.com>
2021-12-24 16:10:14 +0800

    fix-Modify the Todo store

--- src/store/todo.ts
+++ src/store/todo.ts
@@ -1,19 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export interface todoDataType {
+  filter: any;
+  map: any;
+  [index: number]: {
+    id: number,
+    value: string;
+    title: string;
+  },
+};
 export interface todoType {
-  value: string,
+  data: todoDataType,
+  isEdit: boolean,
 };
 
+// console.log(JSON.parse(localStorage.getItem('todoData') as string));
+// const ccc = localStorage.getItem('todoData');
+// JSON.parse(ccc as string);
+// console.log(localStorage.getItem('todoData'))
+
+const todoData: todoDataType | any = [{ id: 1, value: "请输入", title: "请输入内容" }];
+
 const state: todoType = {
-  value: "你好",
+  data: todoData,
+  isEdit: true,
 };
 
 export const todo = createSlice({
   name: "todo",
   initialState: state,
   reducers: {
+    adddata: (state, { payload }) => {
+      const data: any = [
+        {
+          value: payload.value,
+          title: payload.title,
+        },
+      ];
+      localStorage.setItem('todoData', JSON.stringify(data));
+      return { ...state, data };
+    },
     updata: (state, { payload }) => {
-      console.log(payload);
       return { ...state, ...payload };
     },
   },
