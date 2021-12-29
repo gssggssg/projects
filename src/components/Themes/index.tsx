9ef4f1d7743adb81e61e7838ceded3ac62d21e1f
wuweijiange <490104722@qq.com>
2021-12-29 13:26:22 +0800

    fix-Update the theme

--- src/components/Themes/index.tsx
+++ src/components/Themes/index.tsx
@@ -9,7 +9,6 @@ interface Props {
   className?: string;
   theme: string;
   selection?: boolean;
-  scrollBar?: boolean;
 };
 
 interface Themes {
@@ -26,12 +25,10 @@ const themes: Themes = {
  * @param className Card的类名
  * @param theme 主题颜色 dark(黑暗主题) brigth(明亮主题)
  * @param selection 选中文本颜色，默认为true 跟随主题颜色
- * @param scrollBar 浏览器滚动条 默认为 true 跟随主题颜色
  */
 
 const Theme: React.FC<Props> = (props: Props): JSX.Element => {
-  const { theme, className, selection = true, scrollBar = true } = props;
-  scrollBar && import("./scrollBar.scss");
+  const { theme, className, selection = true } = props;
   return (
     <div className={
       `gssgTheme 
