f0c8f52812132f8d099e7c71bbd300ec01fb4b47
wuweijiange <490104722@qq.com>
2021-12-29 09:43:40 +0800

    feat-The new theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import * as React from "react";
+import { connect } from "react-redux";
 import { renderRoutes, RouteConfig } from "react-router-config";
 import routes from "./routes";
 import Navigation from "./pages/Navigation";
 
-const App = () => {
+const App = (props: any) => {
+  const { theme } = props.global;
+  theme === "dark" && import("./theme/dark.scss");
+  theme === "bright" && import("./theme/bright.scss");
   return (
-    <>
+    <div className="">
       <Navigation />
       {renderRoutes(routes as RouteConfig[])}
-    </>
+    </div>
   );
 };
 
-export default App;
+const mapStateToProps = (state: any) => ({
+  global: state.global,
+});
+
+export default connect(mapStateToProps)(App);
--- src/pages/Navigation/index.tsx
+++ src/pages/Navigation/index.tsx
@@ -1,5 +1,7 @@
-import * as React from "react";
+import React, { } from 'react';
 import { connect } from "react-redux";
+// import bright from "../../theme/bright.scss";
+// import dark from "../../theme/dark.scss";
 import "./index.scss";
 
 interface Props {
@@ -10,23 +12,37 @@ interface Props {
         path: string;
       },
     ]
-  }
+  },
+  global: { theme: string };
+  dispatch: any,
 };
 
 const Navigation = (props: Props): JSX.Element => {
-  const { navigation } = props;
+  const { navigation, global } = props;
+  const ChangeTheme = (): void => {
+    let theme = "dark";
+    global.theme === "dark" && (theme = "bright");
+    global.theme === "bright" && (theme = "dark");
+    props.dispatch({
+      type: "global/updata",
+      payload: { theme },
+    });
+  };
   return (
     <nav className="navigation">
       {
         navigation.menu?.length > 0 &&
-        navigation.menu?.map(
-            (item: { title: string, path: string }) => {
-              return (
-                <a key={item.path} href={item.path} > {item.title} </a>
-              );
-            },
+        navigation.menu?.map((item: { title: string, path: string }) => {
+          return (
+            <a key={item.path} href={item.path} > {item.title} </a>
+          );
+        },
         )
       }
+      <div className='ChangeTheme' >
+        <span className={global.theme === "bright" ? "current" : ""} onClick={ChangeTheme}>明</span>
+        <span className={global.theme === "dark" ? "current" : ""} onClick={ChangeTheme}>暗</span>
+      </div>
     </nav>
   );
 };
@@ -34,6 +50,7 @@ const Navigation = (props: Props): JSX.Element => {
 
 const mapStateToProps = (state: any) => ({
   navigation: state.navigation,
+  global: state.global,
 });
 
 export default connect(mapStateToProps)(Navigation);
