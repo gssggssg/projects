ffad10bff7a4dbe0b4bebb6efc36de4a3773e80b
wuweijiange <490104722@qq.com>
2022-01-07 16:08:15 +0800

    fix-Example Change the UUID type

--- src/methods/UUID.ts
+++ src/methods/UUID.ts
@@ -2,10 +2,10 @@
  * guid 生成 Universally Unique IDentifier（通用唯一识别码）的算法函数
  * @return UUID
  */
-const guid = () => {
-  function s4() {
+const guid = (): string => {
+  function s4(): string {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-  }
+  };
   return (s4() + s4() + "-" + s4() + "-" + s4() +
     "-" + s4() + "-" + s4() + s4() + s4());
 };
