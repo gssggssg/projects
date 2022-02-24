2b01bdb6f96654b3e59a11aba3d417cc7a689405
gssggssg <490104722@qq.com>
2022-02-24 23:18:49 +0800

    fix-Modify type error

--- src/type/index.ts
+++ src/type/index.ts
@@ -3,11 +3,11 @@ import { HomeTypes } from "../store/home";
 import { NavigationTypes } from "../store/navigation";
 import { SnakeTypes } from "../store/snake";
 import { TodoTypes } from "../store/todo";
-import { FSItemTypes } from "../store/itemFS";
+import { ItemFSTypes } from "../store/itemFS";
 
 export type GlobalType = GlobalTypes;
 export type HomeType = HomeTypes;
 export type NavigationType = NavigationTypes;
 export type SnakeType = SnakeTypes;
 export type TodoType = TodoTypes;
-export type FSItemType = FSItemTypes;
+export type FSItemType = ItemFSTypes;
