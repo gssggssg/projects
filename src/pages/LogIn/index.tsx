a523bc2ae7bf022119bb1b3301c40044eb50c9f3
gssggssg <490104722@qq.com>
2022-03-07 20:46:12 +0800

    feat-The new page

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
-
+import Input from "../../components/Input";
+import Card from "../../components/Card";
+import Button from "../../components/Button";
+import styles from "./index.module.scss";
 type LogInType = {}
 
 const LogIn: React.FC<LogInType> = (): JSX.Element => {
   return (
-    <div>注册与登录页面</div>
+    <Card width={600}>
+      <>
+        注册与登录页面
+        <form onChange={(e) => console.log(e.target)}>
+          <div className={styles.form}>
+            <ul>
+              <li><Input placeholder="请输入账号" onChange={(first: any) => console.log(first)} /></li>
+              <li><Input placeholder="请输入账号" onChange={(first: any) => console.log(first)} /></li>
+              <li><Input placeholder="请输入账号" onChange={(first: any) => console.log(first)} /></li>
+              <li><Input placeholder="请输入账号" onChange={(first: any) => console.log(first)} /></li>
+            </ul>
+          </div>
+          <Button text="注册" />
+        </form>
+      </>
+    </Card>
   );
 };
 
