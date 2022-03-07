2f79fdcde7be7b9e4e8888c7968f23fb61bf491a
gssggssg <490104722@qq.com>
2022-03-07 22:05:30 +0800

    fix-Add comments

--- src/methods/UUID.ts
+++ src/methods/UUID.ts
@@ -1,8 +1,11 @@
 /**
  * guid 生成 Universally Unique IDentifier（通用唯一识别码）的算法函数
- * @return UUID
- */
+ * @return {number} UUID
+*/
 const guid = (): string => {
+  /**
+   * @return {number} 返回四位数的随机数
+  */
   function s4(): string {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
