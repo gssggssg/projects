f228dfe1407d56d3eda383a9a82e831126735721
gssggssg <490104722@qq.com>
2022-03-09 20:19:23 +0800

    fix-Change the file

--- src/api/logIn.js
+++ src/api/logIn.js
@@ -1,7 +1,6 @@
 import { post } from "../utlis/requert";
 
 export const logInApi = (param) => {
-  // console.log(first)
   // return request("post", "/api/register", param);
   return post("/api/register", param);
 };
