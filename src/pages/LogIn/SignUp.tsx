8fc58c41a79f5f9b09231d287e6e5452533f1f5d
gssggssg <490104722@qq.com>
2022-03-15 21:34:11 +0800

    fix-Changing component location

--- src/pages/LogIn/SignUp.tsx
+++ src/pages/LogIn/SignUp.tsx
@@ -1,11 +1,56 @@
 // 注册
-import * as React from "react";
+import React, { useState } from "react";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import styles from "./index.module.scss";
 
-type SignInType = {}
+type SignInType = {
+  dispatch: any,
+}
 
-const SignUp: React.FC<SignInType> = (): JSX.Element => {
-  return (
+const SignUp: React.FC<SignInType> = (props): JSX.Element => {
+  const [formData, setFormData] = useState({});
+  const formChange = (type: string, value: string): void => {
+    setFormData({ ...formData, [type]: value });
+  };
+
+  const addAccount = (): void => {
+    props.dispatch({
+      type: "logIn/addData",
+      payload: { ...formData },
+    });
+  };
+
+  return (<>
+    <div className={styles.form}>
+      <div>
+        账号:
+        <Input
+          placeholder="请输入账号"
+          onChange={(e: any) => formChange("username", e.target.value)}
+        />
+      </div>
+      <div>
+        邮箱:
+        <Input
+          placeholder="请输入邮件"
+          onChange={(e: any) => formChange("email", e.target.value)}
+        />
+      </div>
+      <div>
+        密码:
+        <Input
+          placeholder="请输入密码"
+          onChange={(e: any) => formChange("password", e.target.value)}
+        />
+      </div>
+      {/* <div>
+      密码: <Input placeholder="请确认密码" onChange={(first: any) => console.log(first)} />
+    </div> */}
+    </div>
+    <Button text="注册" onClick={() => addAccount()} />
     <div>注册页面</div>
+  </>
   );
 };
 
--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,60 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import Input from "../../components/Input";
 import Card from "../../components/Card";
-import Button from "../../components/Button";
 import styles from "./index.module.scss";
+import SignUp from "./SignUp";
 
 type LogInType = {
   dispatch: any,
 }
 
-const LogIn: React.FC<LogInType> = (props): JSX.Element => {
-  const [formData, setFormData] = useState({});
-
-  const formChange = (type: string, value: string): void => {
-    setFormData({ ...formData, [type]: value });
-  };
-
-  const addAccount = (): void => {
-    props.dispatch({
-      type: "logIn/addData",
-      payload: { ...formData },
-    });
-  };
-
+const LogIn: React.FC<LogInType> = (props: LogInType): JSX.Element => {
+  const { dispatch } = props;
   return (
     <div className={styles.blackBg}>
       <Card width={600} >
         <>
           注册与登录页面
-          <div className={styles.form}>
-            <div>
-              账号:
-              <Input
-                placeholder="请输入账号"
-                onChange={(e: any) => formChange("username", e.target.value)}
-              />
-            </div>
-            <div>
-              邮箱:
-              <Input
-                placeholder="请输入邮件"
-                onChange={(e: any) => formChange("email", e.target.value)}
-              />
-            </div>
-            <div>
-              密码:
-              <Input
-                placeholder="请输入密码"
-                onChange={(e: any) => formChange("password", e.target.value)}
-              />
-            </div>
-            {/* <div>
-              密码: <Input placeholder="请确认密码" onChange={(first: any) => console.log(first)} />
-            </div> */}
-          </div>
-          <Button text="注册" onClick={() => addAccount()} />
+          <SignUp dispatch={dispatch} />
         </>
       </Card>
     </div>
