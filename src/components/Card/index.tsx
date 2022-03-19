03ee01a09e4210e724e79ff7a2380ca497249825
gssggssg <490104722@qq.com>
2022-03-19 22:21:06 +0800

    feat-Add parameters to change the default color

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,6 +9,7 @@ interface Props {
   width?: string | number;
   height?: string | number;
   rounded?: string | number;
+  bg?: boolean;
 };
 
 /**
@@ -17,16 +18,18 @@ interface Props {
  * @param width Card的宽，数字类型单位就为px
  * @param height Card的高，数字类型单位就为px
  * @param Rounded Card的圆角，数字类型单位就为px
+ * @param bg:(boolean) Card的背景色，是否存在
  */
 // const Card = (props: Props): JSX.Element => {
 const Card: React.FC<Props> = (props: Props): JSX.Element => {
-  let { style, width, className, height, rounded } = props;
+  let { style, width, className, height, rounded, bg } = props;
   width = typeof width === "string" ? width : `${width}px`;
   height = typeof height === "string" ? height : `${height}px`;
   rounded = typeof rounded === "string" ? rounded : `${rounded}px`;
+  bg = bg || true;
   return (
     <div
-      className={classnames(styles["gssg-card"], className)}
+      className={classnames(styles["gssg-card"], bg && styles["card-bg"], className)}
       style={{ ...style, width, height, borderRadius: rounded }}
     >
       {props.children}
