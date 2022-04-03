d4db37631a665c2f25ce51a3388380feef8f2e97
gssggssg <490104722@qq.com>
2022-04-03 14:53:56 +0800

    feat-New dotenv package

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+require('dotenv')
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
