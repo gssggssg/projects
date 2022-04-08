e88d35879f912c1ed5cb52833bfba367face7499
gssggssg <490104722@qq.com>
2022-04-09 00:23:41 +0800

    feat-Increase the login

--- backEnd/src/controller/user.js
+++ backEnd/src/controller/user.js
@@ -10,9 +10,9 @@ const { sign, verifyToken } = require('../utils/jsonwebtoken');
 // 添加用户
 module.exports.addUser = async (req, res, next) => {
     try {
-        const { userName, passWord, email } = req.body?.user; // 获取传过来数据
         const { through, error } = addUserValidata(req.body?.user); // 验证数据格式是否正确
         if (!through) throw new HttpEexceptions(401, '数据错误', error)
+        const { userName, passWord, email } = req.body?.user; // 获取传过来数据
         const exists = await User.findByPk(userName); // 验证用户是否存在
         if (exists) throw new HttpEexceptions(401, '用户名已存在', '请更换一个用户名')
         const md5PWD = await md5Password(passWord)
@@ -26,7 +26,7 @@ module.exports.addUser = async (req, res, next) => {
                 avatar: null,
             }
             res.status(200).json({
-                status: 220,
+                status: 200,
                 massage: "success",
                 data: {
                     code: 1,
@@ -39,15 +39,30 @@ module.exports.addUser = async (req, res, next) => {
 }
 
 // 用户登录
-module.exports.loginUser = async (req, res) => {
-    const { userName, passWord } = req.body && req.body?.user; // 获取传过来数据
-    // 验证请求数据
-    const { through, error } = loginUserValidata({ userName, passWord })
-
-    // const user = await User.findOne({ where: { userName:`${userName}` } })
-    const user = await User.findAll()
-    // if (!user) throw new HttpEexceptions(401,)
-    console.log(userName, passWord, through, error, user)
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const { through, error } = loginUserValidata(req.body?.user); // 验证数据格式是否正确
+        if (!through) throw new HttpEexceptions(401, '数据错误', error)
+        const { userName, passWord } = req.body?.user; // 获取传过来数据
+        const user = await User.findByPk(userName); // 验证用户是否存在
+        if (!user) throw new HttpEexceptions(401, '用户名不存在！', '请更换用户名重新输入！')
+        const md5PWD = await verifyPassword(user.dataValues.passWord, passWord) //判断密码是否正确
+        if (!md5PWD) throw new HttpEexceptions(401, '密码不正确！', '请更换密码重新输入！')
+        delete user.dataValues.passWord
+        user.dataValues.token = await sign({
+            userName: user.dataValues.userName,
+            email: user.dataValues.email
+        })
+        res.status(200).json({
+            status: 200,
+            massage: "success",
+            data: {
+                code: 1,
+                massage: "登录成功",
+                data: user
+            }
+        })
+    } catch (error) { next(error) } // 整体异常捕获
 }
 
 // 获取用户
