901fa626c2330e3d0720cb7b33462c9bfec33160
gssggssg <490104722@qq.com>
2022-04-08 22:49:25 +0800

    fix-To complete the registration

--- backEnd/src/controller/user.js
+++ backEnd/src/controller/user.js
@@ -8,7 +8,7 @@ const { sign, verifyToken } = require('../utils/jsonwebtoken');
  */
 
 // 添加用户
-module.exports.addUser = async (req, re, next) => {
+module.exports.addUser = async (req, res, next) => {
     try {
         const { userName, passWord, email } = req.body?.user; // 获取传过来数据
         const { through, error } = addUserValidata(req.body?.user); // 验证数据格式是否正确
@@ -17,30 +17,14 @@ module.exports.addUser = async (req, re, next) => {
         if (exists) throw new HttpEexceptions(401, '用户名已存在', '请更换一个用户名')
         const md5PWD = await md5Password(passWord)
         const newUser = await User.create({ userName, passWord: md5PWD, email })
-        // console.log('newUser', newUser, !through, !!exists)
         if (newUser) {
-            // 创建 token
-            // console.log('niha')
-            // let data = {};
-            // data.userName = userName;
-            // data.email = email;
-            // // data.token = await sign(req.body?.user);
-            // data.bio = null;
-            // data.avatar = null;
-            console.log(newUser)
-            res.status(200).json({
-                massage: "创建用户成功！！！",
-                data: newUser.data,
+            res.json({
+                status: 200,
+                massage: "success",
+                data: newUser?.dataValues,
             })
-            // res.json({
-            //     status: 200,
-            //     massage: "success",
-            //     data: newUser.data
-            // })
         }
-    } catch (error) {
-        next(error)
-    }
+    } catch (error) { next(error) }
 }
 
 // 用户登录
