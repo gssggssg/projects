ba345dda9028309038a9ccf65edef88bf7b7aeed
gssggssg <490104722@qq.com>
2022-04-10 21:29:32 +0800

    Eliminate unnecessary content

--- frontEnd-antdPro/config/routes.ts
+++ frontEnd-antdPro/config/routes.ts
@@ -19,40 +19,6 @@
     ],
   },
   {
-    path: '/welcome',
-    name: 'welcome',
-    icon: 'smile',
-    component: './Welcome',
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    component: './Admin',
-    routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-      {
-        component: './404',
-      },
-    ],
-  },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-  },
-  {
-    path: '/',
-    redirect: '/welcome',
-  },
-  {
     component: './404',
   },
 ];
