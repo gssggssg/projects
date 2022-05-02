a7bf7a2df0688786b1d09be72ed69fcb37edbaa2
gssggssg <490104722@qq.com>
2022-05-02 23:04:28 +0800

    fix-Commented code

--- frontEnd-antdPro/src/app.tsx
+++ frontEnd-antdPro/src/app.tsx
@@ -1,19 +1,19 @@
-import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
-import { SettingDrawer } from '@ant-design/pro-layout';
+// import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
+// import { SettingDrawer } from '@ant-design/pro-layout';
 import { PageLoading } from '@ant-design/pro-layout';
-import type { RunTimeLayoutConfig } from 'umi';
-import { history, Link } from 'umi';
-import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
-import { BookOutlined, LinkOutlined } from '@ant-design/icons';
-import defaultSettings from '../config/defaultSettings';
+// import type { RunTimeLayoutConfig } from 'umi';
+// import { history, Link } from 'umi';
+// import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+// import { BookOutlined, LinkOutlined } from '@ant-design/icons';
+// import defaultSettings from '../config/defaultSettings';
 
-const isDev = process.env.NODE_ENV === 'development';
-const loginPath = '/user/login';
+// const isDev = process.env.NODE_ENV === 'development';
+// const loginPath = '/user/login';
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
-export const initialStateConfig = {
-  loading: <PageLoading />,
-};
+// export const initialStateConfig = {
+//   loading: <PageLoading />,
+// };
 
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
