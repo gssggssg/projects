b7b973b3f42290cd8e7adc744023361bd33f17ef
gssggssg <490104722@qq.com>
2022-06-10 21:47:45 +0800

    fix-Modify the

--- frontEnd-antdPro/src/pages/Home/index.tsx
+++ frontEnd-antdPro/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect, history } from 'umi';
+import { connect } from 'umi';
 import { Card, Button, Descriptions } from 'antd';
 import styles from "./index.module.less";
 
