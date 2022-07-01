f64f7724b87a105a522b63c5ddefd17f12d59c13
gssggssg <490104722@qq.com>
2022-07-01 19:42:17 +0800

    feat:Add right menu

--- frontEnd-antdPro/src/pages/Gobang/Panel.tsx
+++ frontEnd-antdPro/src/pages/Gobang/Panel.tsx
@@ -4,9 +4,24 @@ import styles from "./index.module.less";
 
 // 右边面板菜单
 const Panel: React.FC = (props: any) => {
+  const { nextChessPiece } = props.gobang;
+
+  // 棋子颜色
+  const colorPiece = {
+    white: "白色",
+    black: "黑色",
+  }
+
   return (
     <div className={styles.panelBox}>
-     
+      <div className={styles.roundTip}>
+        <span>
+          {colorPiece[nextChessPiece]}
+        </span>
+        <span></span>
+
+      </div>
+
     </div>
   );
 };
