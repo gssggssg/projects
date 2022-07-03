3dd2d42c9a69129362ba67f4bde133b4b3b622ca
gssggssg <490104722@qq.com>
2022-07-03 17:16:12 +0800

    fix-The registration page cannot be displayed

--- frontEnd-antdPro/src/app.tsx
+++ frontEnd-antdPro/src/app.tsx
@@ -62,7 +62,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
             // 如果没有登录，重定向到 login
             if (!initialState?.currentUser &&
                 location.pathname !== loginPath &&
-                location.pathname === '/signUp') {
+                location.pathname !== '/signUp') {
                 history.push(loginPath);
             }
         },
