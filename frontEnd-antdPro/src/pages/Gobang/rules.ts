6585528bb2ac0cee177d644767865d97c34f5e1c
gssggssg <490104722@qq.com>
2022-07-17 18:03:20 +0800

    The first generation of garbage robots

--- frontEnd-antdPro/src/pages/Gobang/rules.ts
+++ frontEnd-antdPro/src/pages/Gobang/rules.ts
@@ -40,18 +40,13 @@ export const getGameRule = () => {
 export const judgeSuccess = (whitePieces: string[], blackPieces: string[], currentPieces: string, allRuleNum: string[][]) => {
 
     // 胜利对象
-    let result = {
-        isVictory: false,
-        winner: ''
-    }
+    let result = { isVictory: false, winner: '' }
 
     // 当前所下棋子赢法
     const currentRuleNum = (party: string[]) => allRuleNum.filter(
-        (item) => {
-            return item.includes(currentPieces)
-        }
+        (item: string[]) => item.includes(currentPieces)
     ).filter(
-        (item) => {
+        (item: string[]) => {
             let result = true
             for (let index = 0; index < item.length; index++) {
                 if (!party.includes(item[index])) {
@@ -82,29 +77,51 @@ export const judgeSuccess = (whitePieces: string[], blackPieces: string[], curre
 
 // 预判下一步应该下哪里
 export const nextStep = (whitePieces: string[], blackPieces: string[], curPiece: string, nextPiece: string, allRuleNum: string[][]): string => {
+
     const piecesObj = { whitePieces, blackPieces }
-    const result = allRuleNum.map(
-        (ruleItem: string[]) => {
-            let isCan = true
-            const result = ruleItem.map(
-                (a: string) => {
-                    if (piecesObj[curPiece + 'Pieces'].includes(a)) {
-                        isCan = false
-                    }
-                    if (piecesObj[nextPiece + 'Pieces'].includes(a)) {
-                        return null
-                    }
-                    return a
-                }
-            )
-            if (!isCan) return ruleItem
-            return result.filter(s => s)
-        }
-    ).filter(s => s.length && s.length !== 5)
 
-    // 按照优先顺序来排序
-    result.sort((a, b) => a.length - b.length)
+    /**
+     * 胜利方式，按照c参数返回有多少种胜利方式
+     * @param curPieces 对手所下的所有棋子
+     * @param nextPieces 己方所下的所有棋子
+     * @returns string[][]
+     */
+    const victoryMethod = (curPieces: string[], nextPieces: string[]) => {
+        const result = allRuleNum.map(
+            (ruleItem: string[]) => {
+                let isCan = true
+                const result = ruleItem.map(
+                    (a: string) => {
+                        if (curPieces.includes(a)) {
+                            isCan = false
+                        }
+                        if (nextPieces.includes(a)) {
+                            return null
+                        }
+                        return a
+                    }
+                )
+                if (!isCan) return ruleItem
+                return result.filter(s => s)
+            }
+        ).filter(s => s.length && s.length !== 5)
 
+        // 按照优先顺序来排序
+        result.sort((a, b) => a.length - b.length)
+        return result
+    }
+
+    // 对手的胜利方式
+    const rivalVictory = victoryMethod(piecesObj[nextPiece + 'Pieces'], piecesObj[curPiece + 'Pieces'])
+    // 己方的胜利方式
+    const ownVictory = victoryMethod(piecesObj[curPiece + 'Pieces'], piecesObj[nextPiece + 'Pieces'])
+
+    let result = ownVictory
+    if (result.length && ownVictory[0]?.length > rivalVictory[0]?.length && rivalVictory[0]?.length < 3) {
+        result = rivalVictory
+    }
+
+    // 返回
     const ultimately = result.length ? result.filter(
         (item: Array<string | null>) => {
             return item.length === result[0].length
@@ -117,18 +134,3 @@ export const nextStep = (whitePieces: string[], blackPieces: string[], curPiece:
 
     return ultimately[Math.floor(ultimately.length * Math.random())]!
 }
-
-// gobang 算法
-/**
- * 列出所有胜利的赢法
- * 将列出的
- */
-
-
-
-/**
- * 优先级
- * 一步 ： 下一步就胜利 -> 对方下一步就赢
- * 两步： 下两步就胜利 -> 下两步必赢 -> 对方下两步必赢
- *        下两步会被挡，对方下两步会被挡
- */
