1e6be128dadcff799dfa229701acdfa729ed0543
gssggssg <490104722@qq.com>
2022-07-18 21:26:53 +0800

    fix-Fixing login issues

--- frontEnd-antdPro/src/services/user/api.ts
+++ frontEnd-antdPro/src/services/user/api.ts
@@ -1,11 +1,13 @@
 // @ts-ignore
 /* eslint-disable */
 import request from '@/utils/request';
+const token = sessionStorage.getItem('token')
 
 /** 获取当前的用户 GET /api/user/get */
 export async function getUser(options?: { [key: string]: any }) {
   return request<{ data: API.CurrentUser; }>('/api/user/get', {
     method: 'GET',
+    headers: { "Authorization": `Token ${token}` },
     ...(options || {}),
   });
 }
