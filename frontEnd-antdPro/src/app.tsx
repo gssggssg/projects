601c133679bc74e6791472e8a3f858a8b631c38c
gssggssg <490104722@qq.com>
2022-07-31 23:51:47 +0800

    fix-Optimization Description Document

--- frontEnd-antdPro/src/app.tsx
+++ frontEnd-antdPro/src/app.tsx
@@ -12,70 +12,72 @@ const loginPath = '/login';
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
-    loading: <PageLoading />,
+  loading: <PageLoading />,
 };
 
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
  * */
 export async function getInitialState(): Promise<{
-    currentUser?: API.CurrentUser;
-    loading?: boolean;
-    fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
+  currentUser?: API.CurrentUser;
+  loading?: boolean;
+  fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
 }> {
-    const fetchUserInfo = async () => {
-        try {
-            const msg = await queryCurrentUser();
-            return msg.data;
-        } catch (error) {
-            history.push(loginPath);
-        }
-        return undefined;
-    };
-
-    // 如果不是登录页面，执行
-    if (history.location.pathname !== loginPath) {
-        const currentUser = await fetchUserInfo();
-        return {
-            fetchUserInfo,
-            currentUser,
-        };
+  const fetchUserInfo = async () => {
+    try {
+      const msg = await queryCurrentUser();
+      return msg.data;
+    } catch (error) {
+      history.push(loginPath);
     }
-    // const currentUser = await fetchUserInfo();
+    return undefined;
+  };
+
+  // 如果不是登录页面，执行
+  if (history.location.pathname !== loginPath) {
+    const currentUser = await fetchUserInfo();
     return {
-        // currentUser,
-        fetchUserInfo,
+      fetchUserInfo,
+      currentUser,
     };
+  }
+  const currentUser = await fetchUserInfo();
+  return {
+    currentUser,
+    fetchUserInfo,
+  };
 }
 
 // // ProLayout 支持的api https://procomponents.ant.design/components/layout
 export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) => {
-    return {
-        // rightContentRender: () => <RightContent />,
-        disableContentMargin: false,
-        waterMarkProps: {
-            content: '四冥',
-        },
-        footerRender: () => <Footer />,
-        onPageChange: () => {
-            const { location } = history;
-            // 如果没有登录，重定向到 login
-            if (!initialState?.currentUser &&
-                location.pathname !== loginPath &&
-                location.pathname !== '/signUp') {
-                history.push(loginPath);
-            }
-        },
-        menuHeaderRender: undefined,
-        // 自定义 403 页面
-        // unAccessible: <div>unAccessible</div>,
-        // 增加一个 loading 的状态
-        childrenRender: (children, props) => {
-            // if (initialState?.loading) return <PageLoading />;
-            return (
-                <>
-                    {children}
-                    {/* {!props.location?.pathname?.includes('/login') && (
+  return {
+    // rightContentRender: () => <RightContent />,
+    disableContentMargin: false,
+    waterMarkProps: {
+      content: '四冥',
+    },
+    footerRender: () => <Footer />,
+    onPageChange: () => {
+      const { location } = history;
+      // 如果没有登录，重定向到 login
+      if (
+        !initialState?.currentUser &&
+        location.pathname !== loginPath &&
+        location.pathname !== '/signUp'
+      ) {
+        history.push(loginPath);
+      }
+    },
+    menuHeaderRender: undefined,
+    // 自定义 403 页面
+    // unAccessible: <div>unAccessible</div>,
+    // 增加一个 loading 的状态
+    childrenRender: (children, props) => {
+      // if (initialState?.loading) return <PageLoading />;
+      return (
+        <>
+          {children}
+          {/* {!props.location?.pathname?.includes('/login') && (
                         <SettingDrawer
                             disableUrlParams
                             enableDarkTheme
@@ -87,18 +89,18 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
                             }}
                         />
                     )} */}
-                </>
-            );
-        },
-    };
+        </>
+      );
+    },
+  };
 };
 
 // 解决控制台输出 http 报错
 export const dva = {
-    config: {
-        onError(e: any) {
-            e.preventDefault();
-            console.error(e.message);
-        },
+  config: {
+    onError(e: any) {
+      e.preventDefault();
+      console.error(e.message);
     },
+  },
 };
