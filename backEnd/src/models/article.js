34ef7d6132031d2f4dce895717fddeae8af6e781
gssggssg <490104722@qq.com>
2023-11-09 20:13:51 +0800

    feat: Data Mapping Table add time

--- backEnd/src/models/article.js
+++ backEnd/src/models/article.js
@@ -1,25 +1,45 @@
-const { DataTypes } = require('sequelize')
+const {DataTypes} = require('sequelize')
 const sequelize = require('../db/sequelize')
 
 // 文章 module
 
-const Article = sequelize.define('article', {
+const Article = sequelize.define('article',
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false, // 是否允许为 null
+      primaryKey: true,
+      autoIncrement: true
+    },
     slug: { // 文章别名(唯一ID)
-        type: DataTypes.STRING, // 数据类型
-        allowNull: false, // 是否允许为 null
-        primaryKey: true, // 是否为主键
+      type: DataTypes.STRING, // 数据类型
+      // allowNull: false, // 是否允许为 null
+      // primaryKey: true, // 是否为主键
     },
     title: { // 文章标题
-        type: DataTypes.STRING,
-        allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     describe: { // 描述
-        type: DataTypes.TEXT,
+      type: DataTypes.TEXT,
     },
     body: { // 内容主题
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-});
+    parent_id: { // 内容主题
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    // timestamp: { //我们自己定义的时间戳字段
+    //   type: DataTypes.DATE,
+    //   defaultValue: Date.now()
+    // }
+  },
+  {
+    createdAt: "createTime", // 将createdAt设为自定义字段createdTime
+    updatedAt: "updatedTime", // 将createdAt设为自定义字段createdTime
+  }
+);
 
 module.exports = Article
--- backEnd/src/models/user.js
+++ backEnd/src/models/user.js
@@ -1,31 +1,37 @@
-const { DataTypes } = require('sequelize')
+const {DataTypes} = require('sequelize')
 const sequelize = require('../db/sequelize')
 
 // 用户个人信息
 
-const User = sequelize.define('user', {
+const User = sequelize.define('user',
+  {
     userName: { // 用户名
-        type: DataTypes.STRING, // 数据类型
-        allowNull: false, // 是否允许为 null
-        primaryKey: true, // 是否为主键
-        unique: 'userName', // 是否可以重复
+      type: DataTypes.STRING, // 数据类型
+      allowNull: false, // 是否允许为 null
+      primaryKey: true, // 是否为主键
+      unique: 'userName', // 是否可以重复
     },
     passWord: { // 密码
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: { // 邮箱
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     avatar: { // 头像
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     bio: { // 简介
-        type: DataTypes.TEXT,
-        allowNull: true,
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
-});
+  },
+  {
+    createdAt: "createTime", // 将createdAt设为自定义字段createdTime
+    updatedAt: "updatedTime", // 将createdAt设为自定义字段createdTime
+  }
+);
 
 module.exports = User;
